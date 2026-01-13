3f63e51d34c2bd1348b457614abc7de68161c4af
kth4778 <cnblue123429@gmail.com>
2026-01-13 14:18:05 +0900

    feat: 초기 백엔드 프로젝트 생성

---
+++ src/main/java/hello/boassebackend/BoasseBackendApplication.java
@@ -0,0 +1,13 @@
+package hello.boassebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoasseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoasseBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/boassebackend/BoasseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.boassebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoasseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
