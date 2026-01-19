08bf0b0f3055b5f71b66a2aca822dcd3f784d692
kth4778 <cnblue123429@gmail.com>
2026-01-19 14:50:05 +0900

    feat: 에러 로직 추가

---
+++ src/main/java/hello/boassebackend/global/controller/HealthController.java
@@ -0,0 +1,13 @@
+package hello.boassebackend.global.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return "Boasse Backend is running successfully! (Powered by Render & TiDB)";
+    }
+}
